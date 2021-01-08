e7d9ecb1033faf28328ac83e2277e480665ad44a
Andrey Avramenko <andreyavr@gmail.com>
2021-01-08 16:36:38 +0200

    bench: add new blas scalar product benchmark
    Initializing library handle every time leads to massive overhead.
    From now, all implementations will accept a handle to initialized library.

--- src/main.cpp
+++ src/main.cpp
@@ -30,4 +30,35 @@ static void BM_CuBLAS_ScalarProductNoHandle(benchmark::State &state)
 }
 BENCHMARK(BM_CuBLAS_ScalarProductNoHandle);
 
+static void BM_CuBLAS_ScalarProduct(benchmark::State &state)
+{
+    constexpr auto n = 1000;
+    auto a = cuda::malloc<double>(n);
+    auto b = cuda::malloc<double>(n);
+    if (!a || !b) {
+        throw std::runtime_error{"Failed to allocate memory"};
+    }
+
+    auto &a_ptr = a.value();
+    auto &b_ptr = b.value();
+
+    cuda::memset<double>(a_ptr, 2.0, n);
+    cuda::memset<double>(b_ptr, 2.0, n);
+
+    auto handle_result = cuda::blas::cublas_handle::create();
+    if (!handle_result) {
+        throw std::runtime_error{"Failed to init cuBlas"};
+    }
+    auto &handle = handle_result.value();
+
+    for (auto _ : state) {
+        auto result = cuda::blas::dot(handle, n, a_ptr, b_ptr);
+        if (!result) {
+            throw std::runtime_error{"Failed to calculate vector sum"};
+        }
+        benchmark::DoNotOptimize(result);
+    }
+}
+BENCHMARK(BM_CuBLAS_ScalarProduct);
+
 BENCHMARK_MAIN();
