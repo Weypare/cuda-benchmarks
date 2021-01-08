b9b8f3432246037b7f8f01ea69645ace77fb449e
Andrey Avramenko <andreyavr@gmail.com>
2021-01-08 16:04:39 +0200

    bench: add blas dot product benchmark

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <stdexcept>
+
+#include <benchmark/benchmark.h>
+
+#include "common.hpp"
+#include "blas.hpp"
+
+static void BM_CuBLAS_ScalarProductNoHandle(benchmark::State &state)
+{
+    constexpr auto n = 1000;
+    auto a = cuda::malloc<double>(n);
+    auto b = cuda::malloc<double>(n);
+    if (!a || !b) {
+        throw std::runtime_error{"Failed to allocate memory"};
+    }
+
+    auto &a_ptr = a.value();
+    auto &b_ptr = b.value();
+
+    cuda::memset<double>(a_ptr, 2.0, n);
+    cuda::memset<double>(b_ptr, 2.0, n);
+
+    for (auto _ : state) {
+        auto result = cuda::blas::dot_no_handle(n, a_ptr, b_ptr);
+        if (!result) {
+            throw std::runtime_error{"Failed to calculate vector sum"};
+        }
+        benchmark::DoNotOptimize(result);
+    }
+}
+BENCHMARK(BM_CuBLAS_ScalarProductNoHandle);
+
+BENCHMARK_MAIN();
