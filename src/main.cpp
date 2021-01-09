9b11b6e4b8dc54db5ef9f505d778efd99c998b22
Andrey Avramenko <andreyavr@gmail.com>
2021-01-09 13:09:38 +0200

    bench: add blas ka + b benchmark

--- src/main.cpp
+++ src/main.cpp
@@ -109,6 +109,42 @@ static void BM_CuBLAS_Add(benchmark::State &state)
 }
 BENCHMARK(BM_CuBLAS_Add);
 
+static void BM_CuBLAS_Kxpb(benchmark::State &state)
+{
+    constexpr auto n = 1000;
+    auto a = cuda::malloc<double>(n);
+    auto b = cuda::malloc<double>(n);
+    if (!a || !b) {
+        throw std::runtime_error{"Failed to allocate memory"};
+    }
+
+    auto &a_ptr = a.value();
+    auto &b_ptr = b.value();
+
+    std::vector<double> host(n, 1.0);
+    if (!cuda::memcpy<double>(a_ptr, host.data(), n, cuda::memcpy_kind::H2D)) {
+        throw std::runtime_error{"Failed to copy memory"};
+    }
+    if (!cuda::memcpy<double>(b_ptr, host.data(), n, cuda::memcpy_kind::H2D)) {
+        throw std::runtime_error{"Failed to copy memory"};
+    }
+
+    auto handle_result = cuda::blas::cublas_handle::create();
+    if (!handle_result) {
+        throw std::runtime_error{"Failed to init cuBlas"};
+    }
+    auto &handle = handle_result.value();
+
+    for (auto _ : state) {
+        auto result = cuda::blas::kapb(handle, n, 5, a_ptr, b_ptr);
+        if (!result) {
+            throw std::runtime_error{"Failed to calculate vector sum"};
+        }
+        benchmark::DoNotOptimize(result);
+    }
+}
+BENCHMARK(BM_CuBLAS_Kxpb);
+
 static void BM_CuBLAS_MatrixAdd(benchmark::State &state)
 {
     constexpr auto n = 100;
